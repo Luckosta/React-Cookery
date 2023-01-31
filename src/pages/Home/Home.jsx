2d4b58575b89d694ff9e28a834690fcf4886f6e2
Leonid Safonov <safonov.lenia2017@yandex.ru>
2023-01-31 11:29:57 +0300

    Search with URLRoute

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,51 @@ import React, { useEffect, useState } from 'react';
 import { getAllCategories } from '../../api';
 import CategoryList from '../../components/CategoryList/CategoryList';
 import Preloader from '../../components/Preloader/Preloader';
+import Search from '../../components/Search/Search';
+import { useSearchParams } from 'react-router-dom';
 
 function Home() {
    const [catalog, setCatalog] = useState([]);
+   const [filteredCatalog, setFilteredCatalog] = useState([]);
+   const [value, setValue] = useState('');
+
+   const [searchParams, setSearchParams] = useSearchParams();
+   const searchValue = searchParams.get('category') || '';
+
+   const handleSearch = (str) => {
+      setFilteredCatalog(
+         catalog.filter((el) =>
+            el.strCategory.toLowerCase().includes(str.toLowerCase())
+         )
+      );
+   };
 
    useEffect(() => {
-      getAllCategories().then((data) => setCatalog(data.categories));
+      getAllCategories().then((data) => {
+         setCatalog(data.categories);
+         setFilteredCatalog(
+            data.categories.filter((item) =>
+               item.strCategory
+                  .toLowerCase()
+                  .includes(searchValue.toLowerCase())
+            )
+         );
+      });
+   }, []);
+
+   useEffect(() => {
+      setSearchParams({ category: searchValue });
    }, []);
 
    return (
-      <>{catalog.length ? <CategoryList catalog={catalog} /> : <Preloader />}</>
+      <>
+         <Search cb={handleSearch} value={value} setValue={setValue} />
+         {catalog.length ? (
+            <CategoryList catalog={filteredCatalog} />
+         ) : (
+            <Preloader />
+         )}
+      </>
    );
 }
 
